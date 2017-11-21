aa40c42bc1b55bc598c05605cb174042b347ec78
ampres15 <andylara11@hotmail.com>
2017-11-20 20:08:02 -0500

    Ingreso de Moldeo de login y review

---
+++ src/models/login.model.ts
@@ -0,0 +1,10 @@
+export interface Vehiculo {
+    id?: String;
+    color: String;
+    fecha: String;
+    hora: String;
+    lugaresDisponibles: number;
+    pLlegada: String;
+    pPartida: String;
+
+}
