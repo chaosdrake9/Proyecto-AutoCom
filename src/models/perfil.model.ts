0a4fad3f20f34d6e00efc1cec97c5249857ef0b1
ampres15 <andylara11@hotmail.com>
2017-11-20 19:58:45 -0500

    Correccion modelos perfil - reserva

---
+++ src/models/perfil.model.ts
@@ -0,0 +1,7 @@
+export interface Perfil {
+    id?: string;
+    clave: string;
+    fechacreacion: string;
+    nombre: string;
+	email: string;
+}
---
+++ src/models/reserva.model.ts
@@ -0,0 +1,5 @@
+export interface Reserva {
+    id: string;
+    lugares: number;
+    pespera: string;
+}
